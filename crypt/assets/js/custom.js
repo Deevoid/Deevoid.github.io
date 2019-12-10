9b0050bf4642834d0cba76e875ff35d5a1d58a15
Dev <dev.4.pandey@gmail.com>
2019-12-11 02:52:09 +0530

    end

---
+++ crypt/assets/js/custom.js
@@ -0,0 +1,7 @@
+function myf() {
+   var x = document.getElementById("sidebar");
+    var y = document.getElementById("overlay");
+    x.classList.toggle("active");
+    y.classList.toggle("active");
+}
+
