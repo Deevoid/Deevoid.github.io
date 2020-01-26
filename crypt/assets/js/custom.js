4df25496617c4765ce879c007410f241329a17ac
Dev <dev.4.pandey@gmail.com>
2020-01-26 16:44:04 +0530

    asd

--- crypt/assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-function myf() {
-   var x = document.getElementById("sidebar");
-    var y = document.getElementById("overlay");
-    x.classList.toggle("active");
-    y.classList.toggle("active");
-}
-
